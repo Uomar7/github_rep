30dccb77de7e1694347b7c536cd5934ecddb37a6
Uomar <uomarearlie7@gmail.com>
2019-05-06 13:16:30 +0300

    service created

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { environment } from  '../environments/environment';
+import { User } from './user';
+import { HttpClient } from '@angular/common/http';
+import { Repos } from './repos';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class UserService {
+    username: string;
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+        // console.log('Github Service Ready...');
+        this.username = 'Uomar7';
+
+    }
+    getUser() {
+        return this.http.get('https://api.github.com/users/' + this.username);
+
+    }
+    getRepos() {
+        return this.http.get(' https://api.github.com/users/' + this.username + '/repos');
+    }
+    updateUser(username: string) {
+        this.username = username;
+    }
 }
