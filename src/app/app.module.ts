77ed504e0ff6d257eafa6b28c9d64ada4845ff13
Uomar <uomarearlie7@gmail.com>
2019-05-06 13:16:03 +0300

    edited modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { HttpClient} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GitlandComponent } from './gitland/gitland.component';
+import { GitlandComponent } from '../app/gitland/gitland.component';
+
 
 @NgModule({
+  exports: [],
   declarations: [
     AppComponent,
-    GitlandComponent
+    GitlandComponent,
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
