6503f8c5ab00f2883d68951610b80c2a5d6270e0
Uomar <uomarearlie7@gmail.com>
2019-05-06 14:04:17 +0300

    styling the heading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { HttpClient} from '@angular/common/http';
+ // import { HttpClient} from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms'
 
